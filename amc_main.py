e6613e445db6f2576b4ad32e2861a56442e0d8e6
bendga <73430432+bendga@users.noreply.github.com>
2023-07-12 17:14:02 +0300

    upload of the working code with bad results
    return main branch to working state

--- amc_main.py
+++ amc_main.py
@@ -1,117 +1,22 @@
 from setup_module import init_setup
-from classification_module import init_classification, combine_accuracy_graphs
+from classification_module import init_classification
 import numpy as np
 
-samples, sample_comb, sample_iq, labels, snr = init_setup()
-
-
-easy_mode = 1
-
-if easy_mode == 1:
-    easy_mods = [
-        "OOK",
-        "4ASK",
-        "BPSK",
-        "QPSK",
-        "8PSK",
-        "16QAM",
-        "AM-SSB-SC",
-        "AM-DSB-SC",
-        "FM",
-        "GMSK",
-        "OQPSK",
-    ]
-    easy_mask = np.isin(labels, easy_mods)
-    labels = labels[easy_mask]
-    snr = snr[easy_mask]
-    samples = samples[easy_mask]
-    sample_comb = sample_comb[easy_mask]
-    sample_iq = sample_iq[easy_mask]
-    easy_mask = np.isin(labels, easy_mods)
-    labels = labels[easy_mask]
-    snr = snr[easy_mask]
+samples, samples_dx, samples_real, samples_imag, labels, snr = init_setup()
+sample_iq = np.concatenate((samples_real, samples_imag), 1)
+sample_comb = np.concatenate((samples, samples_dx), 1)
 
 print("we are here")
 print("classification for cumulants...")
-accuracy_data1 = init_classification(samples, labels, snr, "cumulants")
+init_classification(samples, labels, snr, "cumulants")
 print("classification for cumulants+dx...")
-accuracy_data2 = init_classification(sample_comb, labels, snr, "cumulants+dx")
+init_classification(sample_comb, labels, snr, "cumulants+dx")
 print("classification for cumulants IQ...")
-accuracy_data3 = init_classification(sample_iq, labels, snr, "cumulants_iq")
+init_classification(sample_iq, labels, snr, "cumulants_iq")
 
-combine_accuracy_graphs(accuracy_data1, accuracy_data2, accuracy_data3)
+
 # unique_snr, snr_counts = np.unique(snr, return_counts=True)
 # unique_labels, label_counts = np.unique(labels, return_counts=True)
 
 if __name__ == "__main__":
     filepath = "/home/bendegani/AMC_v2/GOLD_XYZ_OSC.0001_1024.hdf5"
-
-group1 = [
-    "OOK",
-    "AM-SSB-WC",
-    "AM-SSB-SC",
-    "AM-DSB-WC",
-    "AM-DSB-SC",
-    "FM",
-    "GMSK",
-    "OQPSK",
-]  # Low Order and Analog
-group2 = [
-    "4ASK",
-    "8ASK",
-    "16QAM",
-    "32QAM",
-    "64QAM",
-    "128QAM",
-    "256QAM",
-]  # ASK and QAM
-group3 = [
-    "BPSK",
-    "QPSK",
-    "8PSK",
-    "16PSK",
-    "32PSK",
-    "16APSK",
-    "32APSK",
-    "64APSK",
-    "128APSK",
-]  # PSK and APSK
-easy_mods = [
-    "OOK",
-    "4ASK",
-    "BPSK",
-    "QPSK",
-    "8PSK",
-    "16QAM",
-    "AM-SSB-SC",
-    "AM-DSB-SC",
-    "FM",
-    "GMSK",
-    "OQPSK",
-]
-hard_mods = [
-    "OOK",
-    "4ASK",
-    "8ASK",
-    "BPSK",
-    "QPSK",
-    "8PSK",
-    "16PSK",
-    "32PSK",
-    "16APSK",
-    "32APSK",
-    "64APSK",
-    "128APSK",
-    "16QAM",
-    "32QAM",
-    "64QAM",
-    "128QAM",
-    "256QAM",
-    "AM-SSB-WC",
-    "AM-SSB-SC",
-    "AM-DSB-WC",
-    "AM-DSB-SC",
-    "FM",
-    "GMSK",
-    "OQPSK",
-]  # all labels
--- classification_module.py
+++ classification_module.py
@@ -1,12 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
-def init_classification(samples, labels, snr, name, groups):
+def init_classification(samples, labels, snr, name):
+    group1 = [
+        "OOK",
+        "AM-SSB-WC",
+        "AM-SSB-SC",
+        "AM-DSB-WC",
+        "AM-DSB-SC",
+        "FM",
+        "GMSK",
+        "OQPSK",
+    ]  # Low Order and Analog
+    group2 = [
+        "4ASK",
+        "8ASK",
+        "16QAM",
+        "32QAM",
+        "64QAM",
+        "128QAM",
+        "256QAM",
+    ]  # ASK and QAM
+    group3 = [
+        "BPSK",
+        "QPSK",
+        "8PSK",
+        "16PSK",
+        "32PSK",
+        "16APSK",
+        "32APSK",
+        "64APSK",
+        "128APSK",
+    ]  # PSK and APSK
+    groups = [group1, group2, group3]
     x_train, x_test, y_train, y_test, train_indices, test_indices = train_test_split(
         samples,
         labels,
@@ -16,44 +47,13 @@ def init_classification(samples, labels, snr, name, groups):
         stratify=labels,
     )
     snr_test = snr[test_indices]
-    classifier = DecisionTreeClassifier(max_depth=8)
+    classifier = DecisionTreeClassifier()
     classifier.fit(x_train, y_train)
     # y_pred = classifier.predict(x_test)
     # accuracy = accuracy_score(y_test, y_pred)
     # print(f"Accuracy for all: {accuracy:.2f}")
-    # detection_per_snr(x_test, y_test, snr_test, classifier, name)
-    os.makedirs("plots", exist_ok=True)
-    print("Classify per SNR")
-    accuracy_data = detection_per_snr(x_test, y_test, snr_test, classifier, name)
-    print("Classify per Label")
+    detection_per_snr(x_test, y_test, snr_test, classifier, name)
     detection_per_label(x_test, y_test, snr_test, classifier, groups, name)
-
-    return accuracy_data
-
-
-def combine_accuracy_graphs(accuracy_data1, accuracy_data2, accuracy_data3):
-    fig, ax = plt.subplots()
-
-    ax.plot(
-        accuracy_data1["snr"], accuracy_data1["accuracy"], label=accuracy_data1["name"]
-    )
-    ax.plot(
-        accuracy_data2["snr"], accuracy_data2["accuracy"], label=accuracy_data2["name"]
-    )
-    ax.plot(
-        accuracy_data3["snr"], accuracy_data3["accuracy"], label=accuracy_data3["name"]
-    )
-
-    ax.set_xlabel("SNR")
-    ax.set_ylabel("Accuracy")
-    ax.set_title("Accuracy vs SNR")
-    ax.legend()
-
-    # Specify the folder name
-    # Save the figure in the specified folder
-    file_path = os.path.join("plots", "combined_accuracy.png")
-    plt.savefig(file_path)
-    plt.close(fig)
 
 
 def detection_per_snr(x_test, y_test, snr_test, classifier, name):
@@ -66,22 +66,18 @@ def detection_per_snr(x_test, y_test, snr_test, classifier, name):
         y_pred_snr = classifier.predict(x_snr)
         accuracy_snr = accuracy_score(y_snr, y_pred_snr)
         accuracy_list.append(accuracy_snr)
-    # plt.plot(unique_snr, accuracy_list)
-    # plt.xlabel("SNR")
-    # plt.ylabel("Accuracy")
-    # plt.title(f"Accuracy vs SNR for {name}")
-    # file_path = os.path.join('plots', f"accuracy_vs_SNR_{name}.png")
-    return {"snr": unique_snr, "accuracy": accuracy_list, "name": name}
-    # plt.savefig(file_path)
-    # plt.clf()
+    plt.plot(unique_snr, accuracy_list)
+    plt.xlabel("SNR")
+    plt.ylabel("Accuracy")
+    plt.title(f"Accuracy vs SNR for {name}")
+    plt.savefig(f"accuracy_vs_SNR_{name}.png")
+    plt.clf()
 
 
 def detection_per_label(x_test, y_test, snr_test, classifier, groups, name):
     unique_snr = np.unique(snr_test)
     unique_labels = np.unique(y_test)
-    # num_plots = len(unique_labels) // 8  # Number of plots needed
-    num_plots = 3  # Number of plots needed
-
+    num_plots = len(unique_labels) // 8  # Number of plots needed
     fig, axs = plt.subplots(nrows=num_plots, ncols=1, figsize=(10, 6 * num_plots))
     # for i, label_group in enumerate(np.array_split(unique_labels, num_plots)):
     for i, label_group in enumerate(groups):
@@ -94,9 +90,9 @@ def detection_per_label(x_test, y_test, snr_test, classifier, groups, name):
             accuracy_list = []
             for snr_val in unique_snr:
                 mask_snr = snr_label == snr_val
-                x_snr = x_label[mask_snr]
+                X_snr = x_label[mask_snr]
                 y_snr = y_label[mask_snr]
-                y_pred_snr = classifier.predict(x_snr)
+                y_pred_snr = classifier.predict(X_snr)
                 accuracy_snr = accuracy_score(y_snr, y_pred_snr)
                 accuracy_list.append(accuracy_snr)
             axs[i].plot(unique_snr, accuracy_list, label=label)
@@ -104,7 +100,6 @@ def detection_per_label(x_test, y_test, snr_test, classifier, groups, name):
         axs[i].set_ylabel("Accuracy")
         axs[i].legend()
     plt.tight_layout()
-    file_path = os.path.join("plots", f"label_group_accuracy_{name}.png")
-    plt.savefig(file_path)
+    plt.savefig(f"label_group_accuracy_{name}.png")
     print("saved plots")
     plt.clf()
